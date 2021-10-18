a8723606099e8b8ee30199ce0f04973f94e68ee2
yap8 <nebozhenko.arseny@gmail.com>
2021-10-18 21:39:05 +0300

    added responsive menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
-
+document.querySelector('.header__toggle').addEventListener('click', e => {
+  const nav = document.querySelector('.header__nav')
+  
+  if (nav.classList.contains('header__nav--active')) {
+    nav.classList.remove('header__nav--active')
+  } else {
+    nav.classList.add('header__nav--active')
+  }
+})
