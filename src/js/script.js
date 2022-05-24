2499aed84b4c986c6a07c70832544f251f55648f
yap8 <nebozhenko.arseny@gmail.com>
2022-05-24 17:13:59 +0300

    cleaned repo

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.header__toggle').addEventListener('click', e => {
-  const nav = document.querySelector('.header__nav')
-  
-  if (nav.classList.contains('header__nav--active')) {
-    nav.classList.remove('header__nav--active')
-  } else {
-    nav.classList.add('header__nav--active')
-  }
-})
